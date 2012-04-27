830d75846e6e21ddd1a329a34611d100ea7d36cd
Ilya L. Menshikov <unkmas@gmail.com>
2012-04-27 20:58:56 +0600

    update rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,20 @@ require File.expand_path('../../lib/greeb', __FILE__)
 
 RSpec.configure do |c|
   c.mock_with :rspec
+  c.color_enabled = true
 end
 
 RSpec::Matchers.define :be_parsed_as do |expected|
   match do |actual|
-    tree = Greeb::Parser.new(actual).parse
-    tree == expected
+    @tree = Greeb::Parser.new(actual).parse
+    @tree == expected
+  end
+
+  failure_message_for_should do |actual|
+    "expected: #{expected}\ngot:      #{@tree}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "expected: #{expected}\ngot:     #{@tree}"
   end
 end
